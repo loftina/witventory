c1d829410c75557c8c0498f05d71297bff41a68c
RITS <berberi.safet@gmail.com>
2019-11-30 16:03:39 -0500

    removed unneeded css and added fallback messaging to items/res lists for no items

--- Angular/frontend/src/app/item-list/item-list.component.ts
+++ Angular/frontend/src/app/item-list/item-list.component.ts
@@ -4,16 +4,20 @@ import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { CreateReservationFormComponent } from '../create-reservation-form/create-reservation-form.component';
 import { CreateItemFormComponent } from '../create-item-form/create-item-form.component';
+import * as $ from 'jquery';
+
 @Component({
   selector: 'app-item-list',
   templateUrl: './item-list.component.html',
   styleUrls: ['./item-list.component.css']
 })
+
 export class ItemListComponent implements OnInit {
 
 	items = [];
 	pagination = [];
 	page = 1;
+	
 
 	API = 'http://localhost:3000';
 
@@ -30,6 +34,8 @@ export class ItemListComponent implements OnInit {
 					this.page = itemsResp.current_page;
 				})
 		});
+
+		
 	}
 
 	newPage(new_page) {
