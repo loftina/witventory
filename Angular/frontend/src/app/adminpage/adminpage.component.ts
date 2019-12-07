fe9e94b5bccd92c33e3f327f21f6f8c2bdad2bd3
RITS <berberi.safet@gmail.com>
2019-12-07 10:42:04 -0500

    added margin to no reservations banner and added item list to admin page

--- Angular/frontend/src/app/adminpage/adminpage.component.ts
+++ Angular/frontend/src/app/adminpage/adminpage.component.ts
@@ -9,8 +9,30 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./adminpage.component.css']
 })
 
-export class AdminPageComponent {
+export class AdminPageComponent implements OnInit {
+
+  items = [];
+	pagination = [];
+	page = 1;
 	
-constructor(){}  
+
+	API = 'http://localhost:3000';
+	
+constructor(private http: HttpClient, private route: ActivatedRoute){}  
+
+ngOnInit() {
+  this.route.queryParams.subscribe(params => {
+    let all_params = JSON.parse(JSON.stringify(params));
+    all_params.fields = "image type name location description notes _id";
+    this.http.get(`${this.API}/items/1`, {params: all_params})
+      .subscribe((itemsResp: any) => {
+        this.pagination = Array(itemsResp.total_pages).fill(0).map((x,i)=>i+1)
+        this.items = itemsResp.items;
+        this.page = itemsResp.current_page;
+      })
+  });
+
+  
+}
 
 };
