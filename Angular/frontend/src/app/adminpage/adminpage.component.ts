d2232c0b3db131443c8779992787192ddd02027a
RITS <berberi.safet@gmail.com>
2019-12-13 12:17:03 -0500

    updated to admin page

--- Angular/frontend/src/app/adminpage/adminpage.component.ts
+++ Angular/frontend/src/app/adminpage/adminpage.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
  import { ItemInfoComponent} from '../item-info/item-info.component';
  import { ItemListComponent} from '../item-list/item-list.component';
  import {ReservationListComponent} from '../reservation-list/reservation-list.component';
@@ -20,18 +20,30 @@ export class AdminPageComponent implements OnInit {
   items = [];
 	items_pagination = [];
   items_page = 1;
+
   reservations = [];
+  resPagination = [];
+  resPage = 1;
+
   dmgItemsPagination = []
   dmgItemsPage = 1
   dmgItems = []
   dmgPage = 1
+
+  shortage_items = [];
+	shortage_pagination = [];
+	shortage_page = 1;
+
+  damaged_item_reports = [];
+ 	damaged_item_reports_pagination = [];
+	damaged_item_reports_page = 1;
 
   users = [];
 	
 
 	API = 'http://localhost:3000';
 	
-constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router){}  
+constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router,){}  
 
 ngOnInit() {
   this.route.queryParams.subscribe(params => {
@@ -79,6 +91,28 @@ ngOnInit() {
         });
       })
   });
+
+  this.route.queryParams.subscribe(params => {
+    console.log(params);
+    console.log('asd');
+    let all_params = JSON.parse(JSON.stringify(params));
+    this.http.get(`${this.API}/item_damages/1`, {params: params})
+      .subscribe((itemDamagesResp: any) => {
+        this.damaged_item_reports_pagination = Array(itemDamagesResp.total_pages).fill(0).map((x,i)=>i+1)
+        this.damaged_item_reports = itemDamagesResp.item_damages;
+        this.damaged_item_reports_page = itemDamagesResp.current_page;
+
+      })
+  });
+
+  this.route.queryParams.subscribe(params => {
+    this.http.get(`${this.API}/item_shortages/1`)
+      .subscribe((itemsResp: any) => {
+        this.shortage_pagination = Array(itemsResp.total_pages).fill(0).map((x,i)=>i+1)
+        this.shortage_items = itemsResp.item_shortages;
+        this.shortage_page = itemsResp.current_page;
+      })
+  });
   
 }
 
@@ -95,6 +129,86 @@ deleteReservation(id) {
   this.http.delete(`${this.API}/reservations/reservation/` + id)
     .subscribe(() => {
       this.ngOnInit();
+    });
+}
+
+newDmgReportsPage(new_page) {
+  if (this.damaged_item_reports_pagination[0] > new_page) {
+    new_page = this.damaged_item_reports_pagination[this.damaged_item_reports_pagination.length-1]
+  }
+  else if (this.damaged_item_reports_pagination[this.damaged_item_reports_pagination.length-1] < new_page) {
+    new_page = this.damaged_item_reports_pagination[0];
+  }
+  this.route.queryParams.subscribe(params => {
+    let all_params = JSON.parse(JSON.stringify(params));
+    this.http.get(`${this.API}/item_damages/` + String(new_page), {params: params})
+      .subscribe((itemDamagesResp: any) => {
+        this.damaged_item_reports_pagination = Array(itemDamagesResp.total_pages).fill(0).map((x,i)=>i+1)
+        this.damaged_item_reports = itemDamagesResp.item_damages;
+        this.damaged_item_reports_page = itemDamagesResp.current_page;
+      })
+  });
+}
+
+deleteDamagedRequest(id) {
+  this.http.delete(`${this.API}/item_damages/item_damage/` + id)
+    .subscribe(() => {
+      this.ngOnInit();
+    });
+}
+
+deleteAndUpdateDamaged(req_id, item_id) {
+  const headers = new HttpHeaders()
+    .set("Content-Type", "application/json");
+  this.http.patch(`${this.API}/items/item/` + item_id, [{"propName": "damaged_status", "value": "true"}])
+    .subscribe(res => {
+      this.deleteDamagedRequest(req_id);
+    });
+}
+
+
+newItemShortagePage(new_page) {
+  if (this.shortage_pagination[0] > new_page) {
+    new_page = this.shortage_pagination[this.shortage_pagination.length-1]
+  }
+  else if (this.shortage_pagination[this.shortage_pagination.length-1] < new_page) {
+    new_page = this.shortage_pagination[0];
+  }
+  this.route.queryParams.subscribe(params => {
+    this.http.get(`${this.API}/item_shortages/` + String(new_page))
+      .subscribe((itemsResp: any) => {
+        this.shortage_pagination = Array(itemsResp.total_pages).fill(0).map((x,i)=>i+1)
+        this.shortage_items = itemsResp.item_shortages;
+        this.shortage_page = itemsResp.current_page;
+      })
+  });
+}
+
+newResListPage(new_page) {
+  if (this.resPagination[0] > new_page) {
+    new_page = this.resPagination[this.resPagination.length-1]
+  }
+  else if (this.resPagination[this.resPagination.length-1] < new_page) {
+    new_page = this.resPagination[0];
+  }
+  this.route.queryParams.subscribe(params => {
+    let all_params = JSON.parse(JSON.stringify(params));
+    this.http.get(`${this.API}/reservations/` + String(new_page), {params: params})
+      .subscribe((reservationsResp: any) => {
+        this.resPagination = Array(reservationsResp.total_pages).fill(0).map((x,i)=>i+1)
+        this.reservations = reservationsResp.reservations;
+        this.reservations.forEach(reserv => {reserv.start_date=(new Date(reserv.start_date)).toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric' }); reserv.end_date=(new Date(reserv.end_date)).toLocaleTimeString('en-US', { hour: 'numeric', hour12: true, minute: 'numeric' })});
+        this.resPage = reservationsResp.current_page;
+      })
+  });
+}
+
+markResolved(item_id) {
+  this.http.delete(`${this.API}/item_shortages/item_shortage/` + item_id)
+    .subscribe(() => {
+      this.router.navigateByUrl('/RefreshComponent', { skipLocationChange: true }).then(() => {
+        this.router.navigate(['/shortages']);
+      });
     });
 }
 
